f3631ea1dfe7b86320001e49ba9b7ee34d3e3cdb
k <perfectdicky@yahoo.com.hk>
2020-05-13 18:06:16 +0800

    Add Seal of Orichalocus use Spell Zone as Monster Zone Effect

--- effect.h
+++ effect.h
@@ -206,11 +206,11 @@ inline effect_flag operator|(effect_flag flag1, effect_flag flag2)
 //========== Codes ==========
 //////////kdiy/////////
 #define EFFECT_GOD_IMMUNE				    500
+#define EFFECT_ONETURN_IMMUNE				501
+#define EFFECT_ULTIMATE_IMMUNE				502
 #define EFFECT_OVERINFINITE_ATTACK			503
 #define EFFECT_OVERINFINITE_DEFENSE			504
-#define EFFECT_ONETURN_IMMUNE				505
-#define EFFECT_ULTIMATE_IMMUNE				506
-#define EFFECT_MUST_BE_MATERIAL				507
+#define EFFECT_ORICA		                505
 //////////kdiy/////////
 #define EFFECT_IMMUNE_EFFECT                1
 #define EFFECT_DISABLE                      2
